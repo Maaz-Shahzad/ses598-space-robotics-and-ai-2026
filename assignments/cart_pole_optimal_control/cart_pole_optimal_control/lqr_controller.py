4215d9ea59ab03bd2ba04b6132faeb2b36722c64
Maaz-Shahzad <maazsmughal@gmail.com>
2026-02-16 03:09:54 +0000

    Assgn 2 Q and R values

--- assignments/cart_pole_optimal_control/cart_pole_optimal_control/lqr_controller.py
+++ assignments/cart_pole_optimal_control/cart_pole_optimal_control/lqr_controller.py
@@ -35,8 +35,8 @@ class CartPoleLQRController(Node):
         ])
         
         # LQR cost matrices
-        self.Q = np.diag([1.0, 1.0, 1.0, 1.0])  # State cost
-        self.R = np.array([[1.0]])  # Control cost
+        self.Q = np.diag([2.0, 3.0, 10.0, 7.0])  # State cost
+        self.R = np.array([[0.1]])  # Control cost
         
         # Compute LQR gain matrix
         self.K = self.compute_lqr_gain()
